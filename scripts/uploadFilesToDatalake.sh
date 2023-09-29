fe6099ff66a5c2fe7f970dfbb7df62a98d83aa5f
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-09-29 18:19:28 -0300

    nit

--- scripts/uploadFilesToDatalake.sh
+++ scripts/uploadFilesToDatalake.sh
@@ -6,4 +6,4 @@ accountName="dlsdataboss"
 
 az storage fs file upload -s "$basedir/addresses.csv" -p addresses.csv  -f $filesystem --account-name $accountName  --auth-mode login -o none
 az storage fs file upload -s "$basedir/contact.xml" -p contact.xml -f $filesystem --account-name $accountName  --auth-mode login -o none
-az storage fs file upload -s "$basedir/product.json" -p product.json  -f $filesystem --account-name $accountName  --auth-mode login  -o none
+az storage fs file upload -s "$basedir/product.json" -p product.json  -f $filesystem --account-name $accountName  --auth-mode login -o none
