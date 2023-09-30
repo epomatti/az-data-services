0ecc581d32945c4cca703af7ab54ad4759d1916d
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-09-29 23:41:47 -0300

    scala

--- databricks/mssql.scala
+++ databricks/mssql.scala
@@ -2,7 +2,7 @@ val driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
 val connectionProperties = new java.util.Properties()
 connectionProperties.setProperty("Driver", driverClass)
 
-val jdbcUsername = dbutils.secrets.get(scope = "keyvault-managed", key = "mss")
+val jdbcUsername = dbutils.secrets.get(scope = "keyvault-managed", key = "mssqlusername")
 val jdbcPassword = dbutils.secrets.get(scope = "keyvault-managed", key = "mssqlpassword")
 connectionProperties.put("user", s"${jdbcUsername}")
 connectionProperties.put("password", s"${jdbcPassword}")
